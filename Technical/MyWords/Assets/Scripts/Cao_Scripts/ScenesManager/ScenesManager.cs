723c6c24fff51ebac5b20496b13bc171cd206fae
Nguyễn Thanh Cao <caont.fuky@gmail.com>
2016-03-26 16:45:45 +0700

    - Scene Select Mode - Add Singleton

---
+++ Technical/MyWords/Assets/Scripts/Cao_Scripts/ScenesManager/ScenesManager.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScenesManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void bt_JoinHome()
+    { }
+    public void bt_JoinZoo()
+    { }
+    public void bt_JoinSchool()
+    { }
+    public void bt_JoinRestaurant()
+    { }
+}
