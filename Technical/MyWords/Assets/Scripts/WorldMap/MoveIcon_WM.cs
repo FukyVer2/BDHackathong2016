b6da8423968e5e6072d2df2b5c25ec1b01a51a54
tobi994 <viettt.fuky@gmail.com>
2016-03-26 16:58:25 +0700

    + wolrd map + some effect

---
+++ Technical/MyWords/Assets/Scripts/WorldMap/MoveIcon_WM.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using Holoville.HOTween;
+
+public class MoveIcon_WM : MonoBehaviour {
+    private Vector3 postionMoveTo = new Vector3();
+	// Use this for initialization
+	void Start () {
+        Vector3 temp = gameObject.GetComponent<RectTransform>().position;
+        postionMoveTo = new Vector3(temp.x, temp.y + 0.2f);
+        StartAnimationMove();
+	}
+    public void StartAnimationMove()
+    {
+        HOTween.To(gameObject.GetComponent<RectTransform>(), 0.5f, new TweenParms()
+            .Prop("position", postionMoveTo,false)
+            .Loops(-1, LoopType.Yoyo)
+            .Delay(Random.Range(0.1f,0.5f))
+            );
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Technical/MyWords/Assets/Scripts/WorldMap/Title_WM.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+using Holoville.HOTween;
+
+public class Title_WM : MonoBehaviour {
+    public RectTransform objTitle;
+
+    private Vector3 positionTitle =new Vector3();
+    private Vector3 positionTitleMoveTo = new Vector3();
+
+    void Start()
+    {
+        positionTitle = objTitle.position;
+        positionTitleMoveTo = new Vector3(positionTitle.x,positionTitle.y - positionTitle.y/4);
+    }
+
+    [ContextMenu("test move!")]
+	public void PoiterDown()
+    {
+        if(objTitle)
+        {
+            HOTween.To(objTitle, 0.8f, new TweenParms()
+                .Prop("position", positionTitleMoveTo,false)
+                .OnComplete(PoiterUp)
+                );
+        }
+    }
+
+    public void PoiterUp()
+    {
+        HOTween.To(objTitle, 0.8f, new TweenParms()
+                .Prop("position", positionTitle, false)
+                .Delay(0.75f)
+                );
+    }
+}
