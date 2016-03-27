9dbc2bddbffb46c329b682251fde0a8bec6ab06b
tobi994 <viettt.fuky@gmail.com>
2016-03-27 10:55:06 +0700

    +audio

---
+++ Technical/MyWords/Assets/Scripts/HOANG_SCRIPT/PlayAudio.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayAudio : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        SoundManager.Instance.PlaySoundWithType(AudioType.BACKGROND);
+	}
+
+    void OnEnable()
+    {
+        Debug.Log("play");
+        //SoundManager.Instance.PlaySoundWithType(AudioType.BACKGROND);
+    }
+
+    void OnDisnable()
+    {
+        
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
